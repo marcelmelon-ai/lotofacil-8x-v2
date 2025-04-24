fb94274bc996b475570425c840ca9cf1dcb13887
MARCEL MELON <marcelribeiroarticomelon@gmail.com>
2025-04-24 16:51:39 +0000

    agora sim

--- app.py
+++ app.py
@@ -32,7 +32,8 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # ====== MENU DE NAVEGAÇÃO ======
-print(pagina = menu_lateral())
+pagina = menu_lateral()
+print(pagina)
 
 # ====== EXECUÇÃO DA PÁGINA SELECIONADA ======
 if pagina == "🎰 Gerador de Jogos":
