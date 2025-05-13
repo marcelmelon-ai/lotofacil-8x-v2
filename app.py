c128c99d01bff0c547fedcdd195959a408d628ab
MARCEL MELON <marcelribeiroarticomelon@gmail.com>
2025-05-13 18:31:02 +0000

    oi

--- app.py
+++ app.py
@@ -44,7 +44,9 @@ def main():
     try:
         frequencia = calcular_frequencia(resultados)
     # Garantir que as colunas estão no formato correto
-        frequencia["Dezena"] = frequencia["Dezena"].astype(str)  # Converter para string
+        frequencia["Concurso", "Data do sorteio", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8",
+            "D9", "D10", "D11", "D12", "D13", "D14", "D15"] = frequencia["Concurso", "Data do sorteio", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8",
+            "D9", "D10", "D11", "D12", "D13", "D14", "D15"].astype(str)  # Converter para string
         frequencia["Frequência"] = pd.to_numeric(frequencia["Frequência"], errors="coerce")  # Converter para número
         frequencia = frequencia.dropna()  # Remover linhas inválidas
     except KeyError as e:
