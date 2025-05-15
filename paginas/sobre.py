2caca4b39785ecb7f32a3778c9d4b4b303f8e412
MARCEL MELON <marcelribeiroarticomelon@gmail.com>
2025-05-15 13:53:14 +0000

    eeeee

--- paginas/sobre.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def pagina_sobre():
-    """
-    Página com informações sobre o projeto.
-    """
-    st.title("ℹ️ Sobre")
-    st.write("Este é um projeto para análise e geração de jogos da Lotofácil.")
-    st.write("Desenvolvido com Python e Streamlit.")
