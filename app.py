26de4d95dea5a3c4d0395ddd607482f56e69d539
MARCEL MELON <marcelribeiroarticomelon@gmail.com>
2025-05-20 23:09:24 +0000

    corrigir

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ def main():
             )
 
     # Treinamento com feedback
-if arquoilo_feedback:
+if arquivo_feedback:
     st.subheader("🤖 Aprendizado de Máquina")
     df_feedback = pd.read_excel(arquivo_feedback)
     if "Acertos" in df_feedback.columns:
